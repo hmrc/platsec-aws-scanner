d1b4fa53d971c5508330440d212aea9e6f8fc625
Antoine <achatain@users.noreply.github.com>
2021-05-18 13:05:12 +0100

    [PSEC-1218] Feed main with args rather than parsing args inside main (#19)

--- platsec_aws_scanner.py
+++ platsec_aws_scanner.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
+from src.aws_scanner_argument_parser import AwsScannerArgumentParser
 from src.aws_scanner_main import AwsScannerMain
 
 if __name__ == "__main__":
-    AwsScannerMain()
+    AwsScannerMain(AwsScannerArgumentParser().parse_cli_args())
