34452366f60ed9e0e51536e046e046232673962e
Antoine <achatain@users.noreply.github.com>
2021-09-03 18:29:59 +0100

    [PSEC-1108] Remove test reliance of unrelated objects

--- tests/test_json_serializer.py
+++ tests/test_json_serializer.py
@@ -1,22 +1,17 @@
 from tests.aws_scanner_test_case import AwsScannerTestCase
 
-from src.json_serializer import to_json
+from dataclasses import dataclass
+from typing import Callable, Optional
 
-from tests.test_types_generator import create_flow_log_action, task_report
+from src.json_serializer import to_json
 
 
 class TestJsonSerializer(AwsScannerTestCase):
-    def test_serialize_complex_object_with_optional_properties(self) -> None:
-        self.assertEqual(
-            (
-                '{"account": {"identifier": "account_id", "name": "account_name"}, "description": "task", '
-                '"results": {"key": "val"}}'
-            ),
-            to_json(task_report(partition=None)),
-        )
+    def test_serialize_exclude_callable_and_none_properties(self) -> None:
+        self.assertEqual('{"greetings": "Bonjour!"}', to_json(TestJsonSerializer.TestObject()))
 
-    def test_serialize_exclude_callable_properties(self) -> None:
-        self.assertEqual(
-            '{"description": "Create VPC flow log", "status": "not applied", "vpc_id": "2", "log_group_name": "z"}',
-            to_json(create_flow_log_action(vpc_id="2", log_group_name="z", permission_resolver=lambda: "thing")),
-        )
+    @dataclass
+    class TestObject:
+        greetings: str = "Bonjour!"
+        empty: Optional[str] = None
+        func: Callable[[], str] = lambda: "hello"
