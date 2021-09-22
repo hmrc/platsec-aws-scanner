e275e3a23e041ce21fe3bbb1c993d9224a700a7c
jezd-hmrc <jez.daniels@digital.hmrc.gov.uk>
2021-09-22 09:04:44 +0100

    add exception

--- src/clients/aws_cost_usage_client.py
+++ src/clients/aws_cost_usage_client.py
@@ -6,38 +6,36 @@ from botocore.exceptions import BotoCoreError, ClientError
 
 from src.clients import boto_try
 
+class CostUsageException(Exception):
+    pass
+
 
 class AwsCostUsageClient:
+
     def __init__(self, boto_cost_usage: BaseClient):
         self._logger = getLogger(self.__class__.__name__)
         self._cost_usage = boto_cost_usage
 
     def get_aws_cost_usage(self, service: str, dates: dict) -> Dict:
-        search_filter = (
-            {
-                "Dimensions": {
-                    "Key": "SERVICE",
-                    "Values": [
-                        service,
-                    ],
-                    "MatchOptions": ["EQUALS"],
-                }
-            },
-        )
-        time_period = {"Start": dates["from"], "End": dates["to"]}
+        try:
+            search_filter = (
+                {
+                    "Dimensions": {
+                        "Key": "SERVICE",
+                        "Values": [
+                            service,
+                        ],
+                        "MatchOptions": ["EQUALS"],
+                    }
+                },
+            )
+            time_period = {"Start": dates["from"], "End": dates["to"]}
 
-        return boto_try(
-            lambda: self._cost_usage.get_cost_and_usage(
-                TimePeriod=time_period,
-                Filter=search_filter,
-                Granularity="Monthly"
-            ),
-            None,
-            f"unable to fetch cost usage for '{service}'",
-        )
-        # return boto_try(
-        #     lambda: to_bucket_acl(self._s3.get_bucket_acl(Bucket=bucket)),
-        #     BucketACL,
-        #     f"unable to fetch access control list for bucket '{bucket}'",
-        # )
-        # return {}
+            return self._cost_usage.get_cost_and_usage(
+                    TimePeriod=time_period,
+                    Filter=search_filter,
+                    Granularity="Monthly"
+                )
+
+        except (BotoCoreError, ClientError) as err:
+            raise CostUsageException(f"unable to get cost usage data for {service}: {err}")
