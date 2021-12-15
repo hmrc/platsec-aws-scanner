9bc4ca400a9981b333ef28cc618dd06432b7c946
Antoine <achatain@users.noreply.github.com>
2021-12-15 13:32:33 +0000

    [PSEC-1106] List accounts for central logging report

--- src/clients/composite/aws_central_logging_client.py
+++ src/clients/composite/aws_central_logging_client.py
@@ -1,19 +1,22 @@
 from logging import getLogger
-from typing import Optional
+from typing import Optional, List
 
 from src.aws_scanner_config import AwsScannerConfig as Config
 from src.clients.aws_kms_client import AwsKmsClient
 from src.clients.aws_s3_client import AwsS3Client
+from src.clients.aws_organizations_client import AwsOrganizationsClient
 from src.data.aws_s3_types import Bucket
 from src.data.aws_kms_types import Key
+from src.data.aws_organizations_types import Account
 
 
 class AwsCentralLoggingClient:
-    def __init__(self, s3: AwsS3Client, kms: AwsKmsClient):
+    def __init__(self, s3: AwsS3Client, kms: AwsKmsClient, org: AwsOrganizationsClient):
         self._logger = getLogger(self.__class__.__name__)
         self._config = Config()
         self._s3 = s3
         self._kms = kms
+        self._org = org
 
     def get_event_bucket(self) -> Optional[Bucket]:
         bucket_name = self._config.cloudtrail_logs_bucket()
@@ -22,3 +25,6 @@ class AwsCentralLoggingClient:
 
     def get_event_cmk(self) -> Optional[Key]:
         return self._kms.find_key(self._config.cloudtrail_event_key_id())
+
+    def get_all_accounts(self) -> List[Account]:
+        return self._org.get_all_accounts()
--- tests/clients/composite/test_aws_central_logging_client.py
+++ tests/clients/composite/test_aws_central_logging_client.py
@@ -2,11 +2,11 @@ from unittest.mock import Mock
 
 from src.clients.composite.aws_central_logging_client import AwsCentralLoggingClient
 
-from tests.test_types_generator import bucket, key
+from tests.test_types_generator import account, bucket, key
 
 
-def client(s3: Mock = Mock(), kms: Mock = Mock()) -> AwsCentralLoggingClient:
-    return AwsCentralLoggingClient(s3, kms)
+def client(s3: Mock = Mock(), kms: Mock = Mock(), org: Mock = Mock()) -> AwsCentralLoggingClient:
+    return AwsCentralLoggingClient(s3, kms, org)
 
 
 def test_get_event_bucket() -> None:
@@ -33,3 +33,9 @@ def test_get_event_cmk() -> None:
 def test_get_event_cmk_not_found() -> None:
     kms_client = Mock(find_key=Mock(return_value=None))
     assert client(kms=kms_client).get_event_cmk() is None
+
+
+def test_get_all_accounts() -> None:
+    expected_accounts = [account("123456", "test-acc-01"), account("123456", "test-acc-01")]
+    org_client = Mock(get_all_accounts=Mock(return_value=expected_accounts))
+    assert client(org=org_client).get_all_accounts() == expected_accounts
