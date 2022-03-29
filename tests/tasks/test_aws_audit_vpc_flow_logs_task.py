904f5126ecf5a8162d5f7f2d01f6bfa949f34432
Antoine <achatain@users.noreply.github.com>
2022-03-29 11:46:05 +0100

    [PSEC-1546] Add org client into VPC composite client
    Co-authored-by: Freshrojek <jnboyer1@gmail.com>

--- tests/tasks/test_aws_audit_vpc_flow_logs_task.py
+++ tests/tasks/test_aws_audit_vpc_flow_logs_task.py
@@ -17,7 +17,7 @@ from tests.test_types_generator import (
     vpc,
 )
 
-vpc_client = AwsVpcClient(ec2=Mock(), iam=Mock(), logs=Mock(), kms=Mock())
+vpc_client = AwsVpcClient(ec2=Mock(), iam=Mock(), logs=Mock(), kms=Mock(), org=Mock())
 vpcs = [vpc(id="vpc-1"), vpc(id="vpc-2")]
 actions = [delete_flow_log_action(flow_log_id="fl-4"), create_flow_log_action(vpc_id="vpc-7")]
 
