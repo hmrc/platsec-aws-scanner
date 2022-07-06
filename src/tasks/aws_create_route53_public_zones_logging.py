fece8d2e7b8682130f642dc7933b0d51a0728256
ulaskan <15844743+ulaskan@users.noreply.github.com>
2022-07-06 09:54:14 +0100

    refactored create query log method

--- src/tasks/aws_create_route53_public_zones_logging.py
+++ src/tasks/aws_create_route53_public_zones_logging.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from typing import Any, Dict
 
+from botocore.exceptions import BotoCoreError, ClientError
+
 from src.clients.composite.aws_route53_client import AwsRoute53Client
 from src.tasks.aws_audit_route53_public_zones_task import AwsAuditRoute53PublicZonesTask
 from src.data.aws_organizations_types import Account
@@ -18,7 +20,14 @@ class AwsCreateRoute53PublicZonesLogsTask(AwsTask):
         hostedzones = hostedzonesTask._run_task(client)
         for zone in hostedzones.values():
             if zone.queryLog == "":
-                zone.queryLog = "arn:aws:logs:us-east-1:" + self.account.identifier + ":log-group:/aws/route53/" + zone.name
-                client.create_query_logging_config(zone.id, zone.queryLog)
+                query_log_arn = "arn:aws:logs:us-east-1:" + self.account.identifier + ":log-group:/aws/route53/" + zone.name
+                client.create_query_logging_config(zone.id, query_log_arn)
+                zone.queryLog = query_log_arn
+                                
+                # try:
+                #     client.create_query_logging_config(zone.id, query_log_arn)
+                #     zone.queryLog = query_log_arn
+                # except (BotoCoreError, ClientError) as err:
+                #     print(f"\n[-] WARNING: failed to enable the logging for zone, '{zone.name}': {err}\n")
 
         return hostedzones
