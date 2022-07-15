380df25980958b76f4ef9d0939a31a3c2e38a668
cob16 <public+github@cormacbrady.info>
2022-07-15 12:27:18 +0100

    add extra tags to vpc flow logs

--- src/__init__.py
+++ src/__init__.py
@@ -5,5 +5,7 @@ from src.data.aws_common_types import Tag
 
 PLATSEC_SCANNER_TAGS: Sequence[Tag] = [
     Tag(key="allow-management-by-platsec-scanner", value="true"),
-    Tag(key="src-repo", value="https://github.com/hmrc/platsec-aws-scanner"),
+    Tag(key="source-code", value="https://github.com/hmrc/platsec-aws-scanner"),
+    Tag(key="business-unit", value="MDTP"),
+    Tag(key="owner", value="PlatSec"),
 ]
