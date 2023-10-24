90108dcd5a57787814f4deb6a63070dcfcf64264
wunzeco <1915636+wunzeco@users.noreply.github.com>
2023-10-24 10:14:34 +0100

    PSEC-1953 fix bug fix when ssm document does not exist in an account

--- src/tasks/aws_audit_ssm_document_task.py
+++ src/tasks/aws_audit_ssm_document_task.py
@@ -1,9 +1,12 @@
 from dataclasses import dataclass
 import json
+from logging import getLogger
 from typing import Any, Dict
 
 from src.clients.aws_ssm_client import AwsSSMClient
 from src.data.aws_organizations_types import Account
+from src.data.aws_scanner_exceptions import GetSSMDocumentException
+from src.data.aws_ssm_types import SSMDocument
 from src.tasks.aws_ssm_task import AwsSSMTask
 
 SESSION_MANAGER_RUN_SHELL_DOCUMENT_NAME = "SSM-SessionManagerRunShell"
@@ -13,15 +16,21 @@ SESSION_MANAGER_RUN_SHELL_JSON_FILE = "src/resources/SessionManagerRunShell.json
 @dataclass
 class AwsAuditSSMDocumentTask(AwsSSMTask):
     def __init__(self, account: Account, region: str) -> None:
+        self._logger = getLogger(self.__class__.__name__)
         super().__init__(description="Audit SSM RunShell Document", account=account, region=region)
 
     def _run_task(self, client: AwsSSMClient) -> Dict[str, Any]:
-        observed = client.get_document(name=SESSION_MANAGER_RUN_SHELL_DOCUMENT_NAME)
+        try:
+            observed = client.get_document(name=SESSION_MANAGER_RUN_SHELL_DOCUMENT_NAME)
+        except GetSSMDocumentException as e:
+            self._logger.error(e)
+            observed = SSMDocument(schema_version="", description="", session_type="", inputs={})
+
         with open(SESSION_MANAGER_RUN_SHELL_JSON_FILE) as f:
             expected = json.load(f)
 
         try:
-            observed_max_session_duration = int(observed.inputs["maxSessionDuration"])
+            observed_max_session_duration = int(observed.inputs.get("maxSessionDuration", ""))
             max_session_duration_is_compliant = observed_max_session_duration <= int(
                 expected["inputs"]["maxSessionDuration"]
             )
@@ -34,11 +43,11 @@ class AwsAuditSSMDocumentTask(AwsSSMTask):
                     "name": SESSION_MANAGER_RUN_SHELL_DOCUMENT_NAME,
                     "compliancy": {
                         "s3BucketName": {
-                            "compliant": observed.inputs["s3BucketName"] == expected["inputs"]["s3BucketName"],
+                            "compliant": observed.inputs.get("s3BucketName", "") == expected["inputs"]["s3BucketName"],
                             "message": "S3 bucket name should be mdtp-ssm-session-manager-audit-logs",
                         },
                         "s3EncryptionEnabled": {
-                            "compliant": observed.inputs["s3EncryptionEnabled"]
+                            "compliant": observed.inputs.get("s3EncryptionEnabled", False)
                             == expected["inputs"]["s3EncryptionEnabled"],
                             "message": "S3 encryption should be enabled",
                         },
@@ -47,7 +56,7 @@ class AwsAuditSSMDocumentTask(AwsSSMTask):
                             "message": "maxSessionDuration should be less than or equal to 120 mins",
                         },
                         "shellProfile": {
-                            "compliant": observed.inputs["shellProfile"] == expected["inputs"]["shellProfile"],
+                            "compliant": observed.inputs.get("shellProfile", "") == expected["inputs"]["shellProfile"],
                             "message": "shellProfile should match expected config",
                         },
                     },
